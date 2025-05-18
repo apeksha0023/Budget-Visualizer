4a8800f1041501ecd1a0584e5e72d24f77750b03
Apeksha Masodkar <masodkarapeksha@gmai.com>
2025-05-18 13:39:24 +0530

    change in url

--- client/src/services/transactionService.js
+++ client/src/services/transactionService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/transactions";
+const API = "https://budget-visualizer-7767.onrender.com/api/transactions";
 
 export const getTransactions = async () => {
   const res = await axios.get(API);
